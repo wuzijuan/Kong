be67eb4ba8119aa81f2f9663757e200362c406c8
wuzijuan <1347407826@qq.com>
2018-09-13 11:53:11 +0800

    修改了Activity

--- app/src/main/java/com/jiyun/kong/MainActivity.java
+++ app/src/main/java/com/jiyun/kong/MainActivity.java
@@ -1,13 +1,11 @@
 package com.jiyun.kong;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //我第一次改动
     }
 }
